d285afa1f6bded218cf39089e863db029c419170
Thomas Read <thjread@gmail.com>
2019-03-19 22:05:32 +0000

    Change local database to be under postgres user (to make restoring backups work)

--- Backup/restore-local.sh
+++ Backup/restore-local.sh
@@ -1,4 +1,4 @@
 aws s3 cp s3://reminders-backup/$1 $1.sql
-dropdb -h localhost -U reminders db
-createdb -h localhost -U reminders db
-psql -h localhost -U reminders db -f $1.sql
+dropdb -h localhost -U postgres db
+createdb -h localhost -U postgres db
+psql -h localhost -U postgres db -f $1.sql
