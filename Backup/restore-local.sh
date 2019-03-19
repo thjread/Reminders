19601bf6db1d3ae186fdc792ee7cf30740473334
Thomas Read <thjread@gmail.com>
2019-03-19 22:12:53 +0000

    Update backup script

--- Backup/restore-local.sh
+++ Backup/restore-local.sh
@@ -1,4 +1,6 @@
-aws s3 cp s3://reminders-backup/$1 $1.sql
-dropdb -h localhost -U postgres db
-createdb -h localhost -U postgres db
-psql -h localhost -U postgres db -f $1.sql
+#!/bin/sh
+if [ -z "$1" ]; then
+  echo "Please specify a backup to restore (e.g. ./restore-local.sh hourly)"
+  exit
+fi
+aws s3 cp s3://reminders-backup/$1 $1.sql && dropdb -h localhost -U postgres db && createdb -h localhost -U postgres db && psql -h localhost -U postgres db -f $1.sql
