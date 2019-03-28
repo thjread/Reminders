7d8c87a68e59d96be29bd9ecf2ef154e9923a989
Thomas Read <thjread@gmail.com>
2019-03-28 21:34:09 +0000

    Fix m.request error code handling

--- Frontend/reminders/src/models/update.ts
+++ Frontend/reminders/src/models/update.ts
@@ -9,7 +9,7 @@ import { hash } from "./serialize";
 
 declare var API_URI: boolean; // provided by webpack
 
-interface ActionDummy {
+interface SyncActionDummy {
     type: string;
     payload: any;
 }
@@ -56,8 +56,20 @@ export function stateFromStorage(loginDetails: LoginDetails | null = null) {
     return is;
 }
 
+function serverError(actions: SyncActionDummy[]) {
+    if (actions.length > 0) {
+        showMessage(
+            "Server error - offline actions not saved (you may need to close and reopen the webpage)");
+        // give up on actions
+        actions.forEach((a) => store.dispatch(syncActionSynced(a.payload.action_id)));
+    } else {
+        showMessage("Server error (you may need to close and reopen the webpage)");
+    }
+    setOnlineAsOf(undefined);
+}
+
 // action must have an action_id field with a uuidv4 in it
-export function serverUpdate(actions: ActionDummy[]
+export function serverUpdate(actions: SyncActionDummy[]
                              = store.getState().syncActions.asMutable()) {
     const state = store.getState();
     if (state.loginDetails) {
@@ -72,51 +84,55 @@ export function serverUpdate(actions: ActionDummy[]
                     batch: actions,
                     expected_hash: state.hash,
                 },
-            }).then((response: any) => {
-                switch (response.type) {
-                    case "SUCCESS": {
-                        actions.forEach((a) => store.dispatch(syncActionSynced(a.payload.action_id)));
-                        store.dispatch(setOnlineAsOf(new Date()));
-                        break;
+                deserialize: (v) => v, // prevent default JSON deserialization
+            }).then((rawResponse: string) => {
+                let response;
+                try {
+                    response = JSON.parse(rawResponse);
+                } catch (e) {
+                    console.warn("Could not parse response from server");
+                    serverError(actions);
+                }
+
+                if (response) {
+                    switch (response.type) {
+                        case "SUCCESS": {
+                            actions.forEach((a) => store.dispatch(syncActionSynced(a.payload.action_id)));
+                            store.dispatch(setOnlineAsOf(new Date()));
+                            break;
+                        }
+                        case "HASH_MISMATCH": {
+                            const serverHash = response.hash;
+                            const todos = response.todos;
+                            actions.forEach((a) => store.dispatch(syncActionSynced(a.payload.action_id)));
+                            // tslint:disable-next-line:no-console
+                            console.log("Local hash " + state.hash + " does not match hash " +
+                                        serverHash + " from server - updating");
+                            updateWithServerTodos(todos as ServerTodoRow[], serverHash);
+                            store.dispatch(setOnlineAsOf(new Date()));
+                            break;
+                        }
+                        case "INVALID_TOKEN":
+                            showMessage("Authentication error");
+                            logout();
+                            break;
+                        case "EXPIRED_TOKEN":
+                            showMessage("Saved login details expired - please log in again");
+                            logout(false);
+                            break;
+                        default:
+                            console.warn("Unexpected response from server");
+                            serverError(actions);
                     }
-                    case "HASH_MISMATCH": {
-                        const serverHash = response.hash;
-                        const todos = response.todos;
-                        actions.forEach((a) => store.dispatch(syncActionSynced(a.payload.action_id)));
-                        // tslint:disable-next-line:no-console
-                        console.log("Local hash " + state.hash + " does not match hash " +
-                                    serverHash + " from server - updating");
-                        updateWithServerTodos(todos as ServerTodoRow[], serverHash);
-                        store.dispatch(setOnlineAsOf(new Date()));
-                        break;
-                    }
-                    case "INVALID_TOKEN":
-                        showMessage("Authentication error");
-                        logout();
-                        break;
-                    case "EXPIRED_TOKEN":
-                        showMessage("Saved login details expired - please log in again");
-                        logout(false);
-                        break;
-                    default:
-                        showMessage("Server error (you may need to close and reopen the webpage)");
-                        setOnlineAsOf(undefined);
                 }
             }).catch((e: any) => {
-                console.warn("Server responded with error code " + e.code + ": " + e.message);
+                console.warn("Server responded with error " + e.code);
                 if (e.code !== 0 && e.code !== 503 && e.code !== 521 && e.code !== 525) { // got a response from server
                     // 503, 521, 525 all errors produced by Cloudflare when server is down
-                    if (actions.length > 0) {
-                        showMessage(
-                            "Server error - offline actions not saved (you may need to close and reopen the webpage)");
-                        // give up on actions
-                        actions.forEach((a) => store.dispatch(syncActionSynced(a.payload.action_id)));
-                        setTimeout(serverUpdate, 500); // try to reset to server state
-                    } else {
-                        showMessage("Server error (you may need to close and reopen the webpage)");
-                    }
+                    serverError(actions);
+                } else {
+                    setOnlineAsOf(undefined);
                 }
-                setOnlineAsOf(undefined);
             });
         } else {
             m.redraw(); // make sure other UI elements refresh e.g. todos that have reached their deadline
